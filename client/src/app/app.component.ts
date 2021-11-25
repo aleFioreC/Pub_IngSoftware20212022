eee1f296b3eff51519b9914b2ac3d29b14300726
Alessio Fiore <alessio.fiore1995@libero.it>
2021-11-25 11:06:53 +0100

    Add controller, dao, model and connection to database mysql

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GeneralService } from './services/general.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'client';
+
+  headElements = ['id', 'name'];
+
+  elements: any[] = [];
+
+  constructor(private service: GeneralService) {
+  }
+
+  ngOnInit() {
+    this.service.getProducts().subscribe((res: any) => {
+      console.log(res)
+      this.elements = res
+    })
+  }
 }
