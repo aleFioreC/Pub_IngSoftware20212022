23ce9ac80a5b41f7265d1c46d57fa70c74823700
Alessio Fiore <alessio.fiore1995@libero.it>
2021-11-26 12:45:54 +0100

    Add users

---
+++ server/src/main/java/it/unicas/server/controller/TavoloController.java
@@ -0,0 +1,22 @@
+
+package it.unicas.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import it.unicas.server.dao.TavoloDAO;
+import it.unicas.server.model.Tavolo;
+
+@Controller
+public class TavoloController {
+	
+  @Autowired 
+  private TavoloDAO tavoloDAO;
+
+  @GetMapping(path="/allTavoli")
+  public @ResponseBody Iterable<Tavolo> getAllTavoli() {
+    return tavoloDAO.findAll();
+  }
+}
