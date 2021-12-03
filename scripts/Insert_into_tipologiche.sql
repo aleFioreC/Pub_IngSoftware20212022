b9420aa1045498d94e79ac392f7e4a5aa0d46be5
Tommaso Amato <tommaso.amato@laziodisco.it>
2021-12-03 13:23:46 +0100

    aggiunto script sql per Insert_into_tipologiche.sql

---
+++ scripts/Insert_into_tipologiche.sql
@@ -0,0 +1,6 @@
+INSERT INTO `amici`.`tipologia_pagamento` (`id_tipologia_pagamento`, `descrizione`) VALUES ('1', 'Contanti');
+INSERT INTO `amici`.`tipologia_pagamento` (`id_tipologia_pagamento`, `descrizione`) VALUES ('2', 'Bancomat');
+
+INSERT INTO `amici`.`tipologia_stato_ordine` (`id_stato_ordine`, `descrizione`) VALUES ('1', 'Eseguito');
+INSERT INTO `amici`.`tipologia_stato_ordine` (`id_stato_ordine`, `descrizione`) VALUES ('2', 'Consegnato');
+INSERT INTO `amici`.`tipologia_stato_ordine` (`id_stato_ordine`, `descrizione`) VALUES ('3', 'Pagato');
